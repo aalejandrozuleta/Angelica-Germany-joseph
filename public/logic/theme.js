73079eef33313ebeb6e60832218dd5ec5c9f57c1
aalejandrozuleta <jhoitan266@gmail.com>
2024-03-11 13:50:48 -0500

    night mode

--- public/logic/theme.js
+++ public/logic/theme.js
@@ -1,32 +1,32 @@
-const toggleTheme = document.getElementById('toggle-theme');
-const toggleIcon = document.getElementById('toggle-theme-icon');
+const toggleTheme = document.getElementById("toggle-theme");
+const toggleIcon = document.getElementById("toggle-theme-icon");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const themeState = localStorage.getItem('themeState');
+document.addEventListener("DOMContentLoaded", () => {
+  const themeState = localStorage.getItem("themeState");
 
-    if (themeState === 'dark') {
-        enableDarkTheme();
-    } else {
-        disableDarkTheme();
-    }
+  if (themeState === "dark") {
+    enableDarkTheme();
+  } else {
+    disableDarkTheme();
+  }
 });
 
-toggleTheme.addEventListener('click', () => {
-    if (document.body.classList.contains('dark')) {
-        disableDarkTheme();
-    } else {
-        enableDarkTheme();
-    }
+toggleTheme.addEventListener("click", () => {
+  if (document.body.classList.contains("dark")) {
+    disableDarkTheme();
+  } else {
+    enableDarkTheme();
+  }
 });
 
 function enableDarkTheme() {
-    document.body.classList.add('dark');
-    toggleIcon.src = '../assets/icons/summer.png';
-    localStorage.setItem('themeState', 'dark');
+  document.body.classList.add("dark");
+  toggleIcon.src = '/public/assets/night-mode/summer.png';
+  localStorage.setItem("themeState", "dark");
 }
 
 function disableDarkTheme() {
-    document.body.classList.remove('dark');
-    toggleIcon.src = '../assets/icons/moon.png';
-    localStorage.setItem('themeState', 'light');
+  document.body.classList.remove("dark");
+  toggleIcon.src = "/public/assets/night-mode/moon.png";
+  localStorage.setItem("themeState", "light");
 }
